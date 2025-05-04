7fbbf085117eb5d81e84c4923a4094adcf8474dd
Kousalya Potti <kousalyapotti1@gmail.com>
2025-05-04 15:47:44 -0400

    add Product class with initialization method

---
+++ product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self, name, category, brand, price, quantity, description):
+        self.name = name
+        self.category = category
+        self.brand = brand
+        self.price = price
+        self.quantity = quantity
+        self.description = description
